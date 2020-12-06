2f790dae56f95b8d4df22dd4a42bc11a3efacfa0
Leandro Alves <41408250+leoalves100@users.noreply.github.com>
2020-12-06 19:55:53 -0300

    hello world

--- helloworld.go
+++ helloworld.go
@@ -1,20 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
-var y = "leandro" //Em qualquer lugar
+import "fmt"
 
 func main() {
 
-	x := 10 //Apenas em code blocks
+	fmt.Print("Hello World\n")
 
-	fmt.Printf("Result X: %v %T \n", x, x)
-
-	x, z := 20, 30
-	fmt.Printf("Resulte new X: %v %T \n", x, x)
-	fmt.Printf("Resulte new Z: %v %T \n", z, z)
-
-	fmt.Printf("Result var y out code block: %v, Type: %T \n", y, y)
 }
