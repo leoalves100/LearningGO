7385cfbfa08e653c265cb6af5b0ba9725c2e2306
Leandro Alves <leandro@qrpoint.com.br>
2021-06-19 21:14:51 -0300

    switch and switch types

--- ifelse.go
+++ ifelse.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+var tipo interface{}
 func main()  {
 	
 	// Podemos inicializar valores na instrução if
@@ -12,4 +13,35 @@ func main()  {
 	} else {
 	    fmt.Printf("X maior que %v\n",x)
 	}
+
+	//Switch case
+	x := "b"
+
+	switch x {
+	//Case composto
+	case "a","b":
+		fmt.Print("X = a\n")
+		//Continua a execução mesmo após retorna resultado
+		fallthrough
+	case "c":
+		fmt.Print("X = b\n")
+	default:
+		fmt.Print("Case default\n")
+	}
+
+	//Baseado em tipos
+
+	tipo = 23.3
+	
+	switch tipo.(type) {
+		case int:
+			fmt.Print("Xiii é inteiro\n")
+		case float64:
+			fmt.Print("Valor ..\n")
+		case bool:
+			fmt.Print("Xiii a parada é bool\n")
+		case string:
+			fmt.Print("String na área\n")
+	}
+	
 }
